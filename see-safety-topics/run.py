512fbced467509db87d73d6b9b63a8c7f89bb0f8
Roy Hyunjin Han <rhh@crosscompute.com>
2022-12-12 16:47:37 -0500

    Use chaining

--- see-safety-topics/run.py
+++ see-safety-topics/run.py
@@ -2,8 +2,6 @@ import json
 from os import getenv
 from pathlib import Path
 
-from invisibleroads_macros_disk import make_random_folder
-
 
 NAME_LENGTH = 32
 
@@ -20,19 +18,10 @@ equipment = (input_folder / 'equipment.txt').read_text().splitlines()
 weather = (input_folder / 'weather.txt').read_text().splitlines()
 
 
-run_folder = Path(make_random_folder(
-    '../log-safety-briefing/runs', NAME_LENGTH))
-run_input_folder = run_folder / 'input'
-run_input_folder.mkdir()
 options = []
 if 'hot' in weather:
     options.append({'value': 'stay hydrated'})
-with (run_input_folder / 'topics.json').open('wt') as f:
+with (output_folder / 'topics.json').open('wt') as f:
     json.dump({'options': options}, f)
-
-
-batch_name = run_folder.name
-with (output_folder / 'variables.dictionary').open('wt') as f:
-    json.dump({
-        'form': f'http://localhost:7000/a/log-safety-topics/r/{batch_name}/i',
-    }, f)
+with (output_folder / 'topics.txt').open('wt') as f:
+    f.write('')
